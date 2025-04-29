ffbcfc57abb9161d090d56eef99735472e3e09a4
RomanovYehor <yeghor.romanov@gmail.com>
2025-04-29 18:03:09 +0300

    JWT token logic added

---
+++ BACKEND-fastAPI/Auth Utils/jwt_token_handling.py
@@ -0,0 +1,27 @@
+import jwt
+from consts import JWT_SECRET_KEY_TEMP, EXPIERY_TIME_MINUTES, DATE_FORMAT
+from datetime import datetime, timedelta
+
+def generate_jwt(user_ID) -> str:
+    datestamp = datetime.now()
+    print(datestamp)
+    
+    payload = {
+        "user_ID": user_ID,
+        "issued_at": str(datestamp),
+        "expires": str((datestamp) + timedelta(minutes=EXPIERY_TIME_MINUTES))
+    }
+    return jwt.encode(payload, JWT_SECRET_KEY_TEMP, algorithm="HS256")
+
+def extract_payload(token) -> dict:
+    return jwt.decode(token, JWT_SECRET_KEY_TEMP, algorithms=["HS256"])
+
+def check_token_expiery(token) -> bool:
+    payload = extract_payload(token)
+    expiery_date = payload["expires"]
+    expiery_date = datetime.strptime(expiery_date, DATE_FORMAT)
+    datestamp = datetime.now()
+    if datestamp < expiery_date:
+        return True
+    else:
+        return False
