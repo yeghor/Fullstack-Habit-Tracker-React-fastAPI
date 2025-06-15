44c7b571a4bd9221e40e0515bdce162a31d07cd4
RomanovYehor <yeghor.romanov@gmail.com>
2025-06-15 10:39:22 +0300

    Added db error handler decorator in db_utils

--- BACKEND-fastAPI/db_utils.py
+++ BACKEND-fastAPI/db_utils.py
@@ -5,6 +5,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from fastapi import HTTPException
 from typing import Generator
 from sqlalchemy import select, delete
+from functools import wraps
 
 async def get_db():
     db: AsyncSession = session_local()
@@ -22,64 +23,55 @@ async def get_db():
 #     finally:
 #         db.close()
 
-def get_merged_user(user: Users, db: AsyncSession) -> Users:
-    try:
-        return db.merge(user)
-    except SQLAlchemyError:
-        raise HTTPException(status_code=500, detail="Error while working with database (merging user)")
-    except Exception:
-        raise HTTPException(status_code=500, detail="Uknown error occured. PLease, try again later (merging user)")
+def database_error_handler(action: str):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(db: AsyncSession, *args, **kwargs):
+            try:
+                return await func(db, *args, **kwargs)
+            except SQLAlchemyError:
+                raise HTTPException(status_code=500, detail=f"Error while working with database. Action - {action}")
+            except Exception:
+              raise HTTPException(status_code=500, detail=f"Unkown error occured. Please, try again later. Action - {action}")         
+        return wrapper
+    return decorator
 
-def get_merged_habit(habit: Habits, db: AsyncSession):
-    try:
-        habit = db.merge(habit)
-        return habit
-    except SQLAlchemyError:
-        raise HTTPException(status_code=500, detail="Error while working with database (merging habit)")
-    except Exception:
-        raise HTTPException(status_code=500, detail="Uknown error occured. PLease, try again later (merging habit)")
+@database_error_handler(action="Merge user object")
+async def get_merged_user(db: AsyncSession, user: Users, ) -> Users:
+    return db.merge(user)
 
+@database_error_handler(action="Merge habit object")
+async def get_merged_habit(habit: Habits, db: AsyncSession):
+        return db.merge(habit)
+
+
+@database_error_handler(action="Get completed habits")
 async def get_completed_habits(db: AsyncSession):
-    try:
-        return await db.execute(
-            select(Habits)
-            .where(Habits.completed == True)
-            )
-    except SQLAlchemyError:
-        raise Exception("Error while working with database (getting completed habits)")
-    except Exception:
-        raise Exception("Uknown error occured. PLease, try again later (getting completed habits)")
+    return await db.execute(
+        select(Habits)
+        .where(Habits.completed == True)
+        )
 
+@database_error_handler(action="Get expired auth tokens")
 async def get_expired_jwts(UNIX_timestamp: int | float, db: AsyncSession):  
-    try:
-        return await db.execute(
-            select(JWTTable)
-            .where(JWTTable.expires_at < int(UNIX_timestamp))
-        )
-    except SQLAlchemyError:
-        raise Exception("Error while working with database (getting expired jwts)")
-    except Exception:
-        raise Exception("Uknown error occured. PLease, try again later (getting expired jwts)")    
+    return await db.execute(
+        select(JWTTable)
+        .where(JWTTable.expires_at < int(UNIX_timestamp))
+    )   
 
-async def get_latest_completion(db: AsyncSession, habit_id: str, UNIX_timestamp: int | float):
-    try:
-        return await db.execute(
-            select(HabitCompletions)
-            .where(HabitCompletions.habit_id == habit_id)
-            .order_by(HabitCompletions.completed_at.desc())
-        )
-    except SQLAlchemyError:
-        raise Exception("Error while working with database (getting last completion)")
-    except Exception:
-        raise Exception("Uknown error occured. PLease, try again later (getting last completion)")   
-    
+@database_error_handler(action="Delete expired jwts")
 async def delete_expired_jwts(db: AsyncSession, UNIX_timestamp: int | float) -> None:
-    try:
-        return await db.execute(
-            delete(JWTTable)
-            .where(JWTTable.expires_at < int(UNIX_timestamp))
-        )
-    except SQLAlchemyError:
-        raise Exception("Error while working with database (deleting expired jwts)")
-    except Exception:
-        raise Exception("Uknown error occured. PLease, try again later (deliting expired jwts)")    
+    return await db.execute(
+        delete(JWTTable)
+        .where(JWTTable.expires_at < int(UNIX_timestamp))
+    ) 
+
+
+@database_error_handler(action="Get latest habit completion")
+async def get_latest_completion(db: AsyncSession, habit_id: str, UNIX_timestamp: int | float):
+    return await db.execute(
+        select(HabitCompletions)
+        .where(HabitCompletions.habit_id == habit_id)
+        .order_by(HabitCompletions.completed_at.desc())
+    )
+
